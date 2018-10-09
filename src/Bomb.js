66d42d408cb8d2a1a55ff16fa4906030d720e4ad
Cedric David <cedric.david@btopenworld.com>
2018-10-09 14:31:10 +0000

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,18 @@
 // your Bomb code here!
+import React from 'react';
+
+export default class Bomb extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      secondsLeft: props.initialCount
+    }
+  }
+  render() {
+  const mes = this.state.secondsLeft === 0 ? `Boom!` : `${this.state.secondsLeft} seconds left before I go boom!`;
+
+    return (
+      <div>{mes}</div>
+    )
+  }
+};
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,15 @@
 // your ImageSlider code here!
+import React from 'react';
+
+export default  class ImageSlider extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentSlideIndex: 0
+    }
+  }
+  render() {
+    return <div>I am on slide {this.state.currentSlideIndex}</div>
+  }
+}
